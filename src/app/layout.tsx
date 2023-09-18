c011ba30b5e5633150c80dd28ba560d081bef53d
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2023-09-18 20:33:35 +0200

    Removed some of the initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Jost } from 'next/font/google'
 import TRPCProvider from './_trpc/TRPCProvider'
 
-const inter = Inter({ subsets: ['latin'] })
+import '../scss/main.scss'
+
+const jost = Jost({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={jost.className}>
         <TRPCProvider>
           {children}
         </TRPCProvider>
